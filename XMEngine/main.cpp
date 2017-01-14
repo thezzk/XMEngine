c83a4a4a6043525f966af1fe3e4f0b0b074caf19
thezzk <thezzk@qq.com>
2017-01-14 18:13:18 +0800

    Initial Commit

---
+++ XMEngine/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  XMEngine
+//
+//  Created by thezzk on 17/1/14.
+//  Copyright © 2017年 thezzk. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
